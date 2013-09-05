5ceb2165dc0170981c3307c4b03a4ab257f1b4e8
Mohit Athwani <mohit@athwani.net>
2013-09-05 23:31:21 +0530

    2

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
+var express = require("express");
+var app = express();
+app.use(express.logger());
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 // Local includes
 easyrtcCfg  = require('./config');          // All server configuration (global)
 var g       = require('./lib/general');     // General helper functions
